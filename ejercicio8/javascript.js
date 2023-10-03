7925bd24fffddbe6f3b33f7fc4deaa094bee03df
pauss12 <paulamendezsv@gmail.com>
2023-10-03 12:29:10 +0200

    Cambios 12:28 del dia 3/10

--- ejercicio8/javascript.js
+++ ejercicio8/javascript.js
@@ -1,25 +1,19 @@
-const calcular = () => {
+function calcular() {
     
-    var figura = document.getElementById("figura").value;
+    var figura = document.getElementsByName("figura").value;
     var base = parseFloat(document.getElementById("base").value);
     var altura = parseFloat(document.getElementById("altura").value);
-    let resultado;
+    var resultado;
 
-    if (figura === "rectangulo")
+    if (figura == "rectangulo")
     {
-        resultado = num1 * num2;
-    }else if (figura === "triangulo")
+        resultado = base * altura;
+    }else if (figura == "triangulo")
     {
-        resultado = (num1 * num2) / 2;
+        resultado = (base * altura) / 2;
     } else {
         resultado = "Resultado no valido";
     }
     //document.getElementById("resultado").value = resultado;
     alert(resultado);
-};
-
-const borrar = () => {
-    document.getElementById("base").value = "";
-    document.getElementById("altura").value = "";
-    document.getElementById("resultado").value = "";
-};
+}
