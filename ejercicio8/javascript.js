a6b63cc7b7431a7cade69400bed8201faa1dc58d
pauss12 <paulamendezsv@gmail.com>
2023-10-05 09:29:02 +0200

    nuevos cambios

--- ejercicio8/javascript.js
+++ ejercicio8/javascript.js
@@ -1,19 +1,18 @@
 function calcular() {
     
-    var figura = document.getElementsByName("figura").value;
-    var base = parseFloat(document.getElementById("base").value);
-    var altura = parseFloat(document.getElementById("altura").value);
-    var resultado;
+    let base = parseFloat(document.getElementById("base").value);
+    let altura = parseFloat(document.getElementById("altura").value);
+    let resultado;
 
-    if (figura == "rectangulo")
+    if (document.getElementById("rectangulo").checked)
     {
         resultado = base * altura;
-    }else if (figura == "triangulo")
+    } else if (document.getElementById("triangulo").checked)
     {
         resultado = (base * altura) / 2;
     } else {
         resultado = "Resultado no valido";
     }
-    //document.getElementById("resultado").value = resultado;
-    alert(resultado);
+    document.getElementById("resultado").value = resultado;
+    //alert(resultado);
 }
