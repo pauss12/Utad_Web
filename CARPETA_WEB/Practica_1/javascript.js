08af52914aa9d0df5030ea2bf44a621e7af772da
pauss12 <104299317+pauss12@users.noreply.github.com>
2023-10-22 18:42:10 +0200

    Cambios

--- CARPETA_WEB/Practica_1/javascript.js
+++ CARPETA_WEB/Practica_1/javascript.js
@@ -33,7 +33,7 @@ function Seleccionar() {
         console.log("No hay jugadores seleccionados");
     } else {
         if (jugadorSelecccionado == 0) {
-            //cambiar el texto del boton
+            
             document.getElementById("botonSeleccionado").innerText = "0 Seres Humanos";
         } else if (jugadorSelecccionado == 1) {
             document.getElementById("botonSeleccionado").innerText = "1 Ser Humano";
@@ -41,6 +41,15 @@ function Seleccionar() {
             document.getElementById("botonSeleccionado").innerText = "2 Seres Humanos";
         }
     }
+
+    if (jugadorSelecccionado == null) {
+        document.getElementById("botonJugar").hidden = true;
+        document.getElementById("botonReiniciar").hidden = true;
+    } else {
+        document.getElementById("botonJugar").hidden = false;
+        document.getElementById("botonReiniciar").hidden = false;
+    }
+    
     
 }
 
@@ -89,10 +98,8 @@ function determinarGanador(jugada1, jugada2) {
 //CODIGO PARA JUGAR EL JUEGO ---------------------------------------------------------------------------
 function Jugar() {
 
-    //Ya tengo los jugadores seleccionados, ahora tengo que empezar la partida
     if (jugadorSelecccionado == 0)
     { 
-        //Significa que va a haber 2 maquinas, 0 seres humanos
         jugadaMaquina1 = generarJugada();
         jugadaMaquina2 = generarJugada();
 
@@ -154,11 +161,9 @@ function Jugar() {
 
 function mostrarResultado(ganador) {
 
-    // Establecer la propiedad `hidden` del elemento HTML a `false`
     const elemento = document.getElementById("resultado");
     elemento.hidden = false;
 
-    // Establecer el valor del elemento HTML
     let input = "";
     
     if (ganador == "Gana el jugador 1") {
@@ -170,45 +175,66 @@ function mostrarResultado(ganador) {
         input = "¡¡EMPATE!!";
     }
 
-    // Actualizar la interfaz
     document.getElementById("resultado").value = input;
+
+    //Actualizar contadores
+    actualizarContadores(ganador);
+}
+
+function actualizarContadores(jugador){ 
+
+    if (jugador == "¡¡GANA EL JUGADOR 1!!") {
+        document.getElementById("contadorJugador1").value++;
+    } else if (jugador == "¡¡GANA EL JUGADOR 2!!") {
+        document.getElementById("contadorJugador2").value++;
+    }
 }
 
 // Función para reiniciar el juego -----------------------------------------------------------------------
 function Reiniciar() {
-    // Resetear las variables
+
     ganador = "";
     jugadaMaquina1 = "";
     jugadaMaquina2 = "";
     jugada1 = "";
     jugada2 = "";
 
+    //Reiniciar los contadores
+    document.getElementById("contadorJugador1").value = 0;
+
     // Actualizar la interfaz
     document.getElementById("resultado").hidden = true;
+    document.getElementById("botonJugar").hidden = true;
+    document.getElementById("botonReiniciar").hidden = true;
     document.getElementById("jugador1img").src = "./images/carta_sin_revelar.jpg";
     document.getElementById("jugador2img").src = "./images/carta_sin_revelar.jpg";
 }
 
+function hacer() { 
+
+    setTimeout(function () {
+        Reiniciar();
+    }, 10000);
+}
+
+
 // Función para mostrar las cartas -----------------------------------------------------------------------
 function MostrarJugada(jugada1, jugada2) {
-    // Obtener la imagen correspondiente a la jugada 1
+
     const imgJugada1 = document.getElementById("jugador1img");
 
-    // Modificar la imagen
     imgJugada1.src = mostrarJugada1(jugada1);
 
-    // Obtener la imagen correspondiente a la jugada 2
     const imgJugada2 = document.getElementById("jugador2img");
 
-    // Modificar la imagen
     imgJugada2.src = mostrarJugada1(jugada2);
 }
 
 function mostrarJugada1(jugada) {
-    // Obtener la imagen correspondiente a la jugada
+
     switch (jugada) {
         case "piedra":
-            return "./images/piedra.jpg";
+            return "./images/piedra.png";
         case "papel":
             return "./images/papel.png";
         case "tijera":
