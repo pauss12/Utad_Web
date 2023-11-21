2d8355ca545b0dcc58ea3862245c9eca3f42d46a
pauss12 <paulamendezsv@gmail.com>
2023-11-21 12:18:59 +0100

    cambiar la ruta

--- NEXT/my_app/app/layout.js
+++ NEXT/my_app/app/layout.js
@@ -2,7 +2,8 @@ import { Inter } from 'next/font/google'
 import './globals.css'
 
 import Link from 'next/link'
-import Navbar from '@/components/Navbar';
+import Navbar from '../components/Navbar';
+
 export default function RootLayout({ children }) {
   return (
     <html>
