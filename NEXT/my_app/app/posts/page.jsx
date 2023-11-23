7c58619cd42434942fff127827d09090ba617132
pauss12 <paulamendezsv@gmail.com>
2023-11-23 11:39:26 +0100

    postCard incluido

--- NEXT/my_app/app/posts/page.jsx
+++ NEXT/my_app/app/posts/page.jsx
@@ -1,9 +1,15 @@
+//Desde import alias
+import PostCard from '@/components/PostCard'
+
+//Desde CSS
+//import './Posts.css'
+
 async function loadPosts() {
     const res = await fetch('https://jsonplaceholder.typicode.com/posts')
     const data = await res.json()
     return data
 }
-
+/*
 async function PostPages() {
     const posts = await loadPosts()
     return <div>
@@ -11,10 +17,28 @@ async function PostPages() {
         posts.map(post => (
                 <div key={post.id}>
                <h3>{post.id}. {post.title}</h3>
-                    <p>{post.body}</p></div>
+                <p>{post.body}</p></div>
+        
             ))
     }
     </div>
+
+}
+*/
+
+async function PostPages() {
+
+    const posts = await loadPosts()
+
+    return (
+        <div className='grid'>
+            {
+                posts.map(post => (
+                    <PostCard post={post} key={post.id} />
+                ))
+            }
+        </div>
+    )
 
 }
 
---
+++ NEXT/my_app/components/PostCard.jsx
@@ -0,0 +1,28 @@
+"use client"
+
+//posterior
+import Link from 'next/link'
+
+async function PostCard({ post }) {
+
+    return (
+        <div className="bg-gray-950 p-10">
+            {
+                <div>
+                    <Link href={`/posts/${post.id}`}>
+                        <h3 className='text-xl font-bold mb-4'>{post.id}. {post.title}</h3>
+                    </Link>
+                    <p className='text-slate-300'>{post.body}</p>
+                    <button onClick={() => {
+                        alert('Click ok!')
+                    }}>
+                        Click
+                    </button>
+                </div>
+            }
+        </div>
+    )
+
+}
+
+export default PostCard
