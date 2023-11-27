d68b169e10c2052a109080e781c8757c881f84f4
pauss12 <paulamendezsv@gmail.com>
2023-11-27 19:40:45 +0100

    Eliminar un comentario

--- REACT/practica_2_react/src/componentes/Buscador.jsx
+++ REACT/practica_2_react/src/componentes/Buscador.jsx
@@ -3,23 +3,7 @@ import '../styles/Buscador.css';
 
 import React, { useState } from 'react';
 
-/*
-<div className="input-group mb-3">
-              <span className="input-group-text">Buscador</span>
-              <input
-                type="text"
-                className="form-control"
-                id="floatingInputGroup1"
-                placeholder="nota"
-                value={searchTerm}
-                onChange={(e) => setSearchTerm(e.target.value)}
-              />
-            </div>
-*/ 
-
 function Buscador(props) {
-
-
 
     return (
             <div className="input-group mb-3">
@@ -29,7 +13,13 @@ function Buscador(props) {
                     className="form-control"
                     id="floatingInputGroup1"
                     placeholder="nota"
-                />
+                    value={props.searchTerm}
+                    onChange={(e) => props.handleSearch(e.target.value)}
+
+            
+            />
+            
+
             </div>
     );
 }
