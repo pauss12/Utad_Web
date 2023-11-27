e516d6f848e5a1dfcb0b93ec7d949c6325598064
pauss12 <paulamendezsv@gmail.com>
2023-11-27 09:21:53 +0100

    He eliminado el buscador

--- REACT/practica_2_react/src/componentes/Buscador.jsx
+++ REACT/practica_2_react/src/componentes/Buscador.jsx
@@ -5,43 +5,11 @@ import React, { useState } from 'react';
 
 function Buscador(props) {
 
-    const [searchTerm, setSearchTerm] = useState('');
-    const { tasks } = props;
-
-    // Function to handle input change and filter notes
-    const handleChange = (event) => {
-        const searchTerm = event.target.value;
-        setSearchTerm(searchTerm);
-
-        if (searchTerm === '') {
-            // No search term, display all notes
-            props.setFilteredNotes(tasks);
-        } else {
-            const filteredNotes = tasks.filter((task) => {
-                return (
-                    task.titulo.toLowerCase().includes(searchTerm.toLowerCase()) ||
-                    task.contenido.toLowerCase().includes(searchTerm.toLowerCase())
-                );
-            });
-
-            // Update filtered notes state
-            props.setFilteredNotes(filteredNotes);
-        }
-    };
-
+    
 
     return (
-        <div className="input-group mb-3" >
-            <span className="input-group-text height-min" >Buscar</span>
-            <div className="form-floating">
-                <input type="text"
-                    className="height-min form-control"
-                    placeholder="Buscar:"
-                    onChange={handleChange}
-                    value={searchTerm}
-                />
-            </div>
-        </div>
+        
+
     );
 }
 
