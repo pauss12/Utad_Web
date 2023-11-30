ece3a16d3b038a34dbf15e365a86597290b96270
pauss12 <paulamendezsv@gmail.com>
2023-11-30 19:23:43 +0100

    he creado los inputs circulares

--- NEXT/practica_3_next/src/app/page.js
+++ NEXT/practica_3_next/src/app/page.js
@@ -4,20 +4,27 @@ export default function Page() {
     return (
       
     <> 
-            <div className="contenedor-formulario"
-                style={{
-                    display: 'flex',
-                    flexDirection: 'column',
-                    marginTop: '40px',
-                    padding: '20%',
-                    border: '1px solid white',
-                    width: '40%',
-                    height: '70%',
-                    margin: 'auto',
-                }}>
+        <div className="contenedor-formulario"
+                
+            style={{
+                display: 'flex',
+                flexDirection: 'column',
+                border: '1px solid white',
+                width: '40%',
+                height: '70%',
+                margin: '0 auto',
+            }}>
+                
+            <div className="form-check form-check-inline">
+                    
+                <input className="form-check-input" type="radio"  value="option1" /> Administrador
+                <input className="form-check-input" type="radio"  value="option2" /> Comercio
+                <input className="form-check-input" type="radio" value="option3" /> Usuario REGISTRADO
+                <input className="form-check-input" type="radio" value="option4" /> Usuario NO REGISTRADO
+                    
+            </div>
 
-            hola
-      </div>
+        </div>
     </>
   )
 }
