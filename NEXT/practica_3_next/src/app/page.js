04a5a2e3d88d7dbc0a9bc0ec4ace0ff5d73e7e9c
pauss12 <paulamendezsv@gmail.com>
2023-11-30 19:15:47 +0100

    cambios en la nueva app

---
+++ NEXT/practica_3_next/src/app/page.js
@@ -0,0 +1,23 @@
+import Image from 'next/image'
+
+export default function Page() {
+    return (
+      
+    <> 
+            <div className="contenedor-formulario"
+                style={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    marginTop: '40px',
+                    padding: '20%',
+                    border: '1px solid white',
+                    width: '40%',
+                    height: '70%',
+                    margin: 'auto',
+                }}>
+
+            hola
+      </div>
+    </>
+  )
+}
