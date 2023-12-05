a305e3b893f200c7290b69570ddb239bc5453c08
pauss12 <paulamendezsv@gmail.com>
2023-12-05 12:30:39 +0100

    creo contenedor para el registro de comercios

--- NEXT/practica_3_next/src/app/administrador/page.jsx
+++ NEXT/practica_3_next/src/app/administrador/page.jsx
@@ -1,9 +1,26 @@
+/*
+se podrá registrar un comercio
+(por un admin) enviando la siguiente información:
+- Nombre del comercio.
+- CIF
+- Dirección
+- E-mail
+- Teléfono de contacto
+*/
+
 function Administrador() { 
 
     return (
-        <div>
-            <h1>Administrador</h1>
-        </div>
+
+        <>
+            <div>
+                <h1>Bienvenido Administrador</h1>
+            </div>
+
+            <div className="crear_comercio">
+                
+            </div>
+        </>
     );
 }
 
