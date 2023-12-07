1ff6f5e649ff0af3a1414be15d08036da7b08941
pauss12 <paulamendezsv@gmail.com>
2023-12-07 20:42:17 +0100

    inicio sesion decoracion terminado

--- NEXT/practica3_next_final/app/iniciar_sesion/page.jsx
+++ NEXT/practica3_next_final/app/iniciar_sesion/page.jsx
@@ -50,12 +50,12 @@ function iniciar_sesion() {
             <div className=" min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
                 <div className="sm:mx-auto sm:w-full sm:max-w-sm">
 
-                    <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+                    <h2 className=" text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
                         Sign in
                     </h2>
                 </div>
 
-                <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+                <div className=" sm:mx-auto sm:w-full sm:max-w-sm">
                     <form className="space-y-6" action="#" method="POST">
                         <div>
                             Usuario
