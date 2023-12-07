47e6c4a733ae53be9484205d069c709b9ef6cf38
pauss12 <paulamendezsv@gmail.com>
2023-12-07 11:09:44 +0100

    Nuevo proyecto, pero con tailwind

---
+++ NEXT/practica3_next_final/app/page.js
@@ -0,0 +1,17 @@
+import Link from 'next/link'
+
+export default function Home() {
+
+  return (
+
+    <>
+
+      <h1 className="text-danger">WEB PRACTICA</h1>
+
+      <Link href="./registro_usuario" className="btn btn-primary">Registro</Link>
+
+      <Link href="./iniciar_sesion" className="btn btn-primary">Iniciar sesion</Link>
+    </>
+
+  );
+}
