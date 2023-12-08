ba3f0dbc5f4140258736bfdbd37a71c62fd90d5f
pauss12 <paulamendezsv@gmail.com>
2023-12-08 17:31:33 +0100

    añadir los buscadores que hay en la pagina de anonimo

--- NEXT/practica3_next_final/app/anonimo/page.jsx
+++ NEXT/practica3_next_final/app/anonimo/page.jsx
@@ -1,9 +1,68 @@
 function anonimo() {
 
+    //Busquedas por ciudad
+    const [ComerciosPorCiudad, setCiudad] = useState("");
+
+    //Busquedas por actividad y ciudad
+    const [ComerciosPorActividad, setActividad] = useState("");
+
+    //Busquedas por id
+    const [ComerciosPorId, setId] = useState("");
+
     return (
-        <div>
-            <h1>anonimo</h1>
-        </div>
+        <>
+            <div className="busquedaCiudad">
+                <Buscador ComerciosPorCiudad={ComerciosPorCiudad} />
+            </div>
+
+            <div className="listaComerciosCiudad">
+
+                {tasks.map((comercio) => (
+                    <div key={comercio.id} className="unComercio">
+                        <p>{comercio.nombre}</p>
+                        <p>{comercio.ciudad}</p>
+                    </div>
+                ))}
+            </div>
+
+            <div className="busquedaCiudad_Actividad">
+                <Buscador ComerciosPorActividad={ComerciosPorActividad} />
+            </div>
+
+            <div className="listaComerciosActividad">
+
+                {tasks.map((comercio) => (
+
+                    <>
+                        <div key={comercio.id} className="unComercio">
+                            <p>{comercio.nombre}</p>
+                            <p>{comercio.actividad}</p>
+                        </div>
+                    </>
+
+                ))}
+            </div>
+
+            <div className="busquedaPorId">
+                <Buscador ComerciosPorId={ComerciosPorId} />
+            </div>
+
+            <div className="listaComerciosId">
+
+                {tasks.map((comercio) => (
+
+                    <>
+
+                        <div key={comercio.id} className="unComercio">
+                            <p>{comercio.nombre}</p>
+                            <p>{comercio.id}</p>
+                        </div>
+                    </>
+
+                ))}
+            </div>
+            
+        </>
     );
 }
 
