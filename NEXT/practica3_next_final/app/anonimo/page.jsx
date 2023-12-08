20c3ce80190dfaa354e6a1bc4038eb5775f692c4
pauss12 <paulamendezsv@gmail.com>
2023-12-08 23:04:22 +0100

    Boton para ir a la pagina para registrarse

--- NEXT/practica3_next_final/app/anonimo/page.jsx
+++ NEXT/practica3_next_final/app/anonimo/page.jsx
@@ -2,44 +2,9 @@
 
 import React, { useState } from 'react';
 import Buscador from '../componentes/Buscador.jsx';
+import Link from 'next/link';
 
 import "../styles/anonimo.css"
-
-/*
-
-<div className="lista">
-
-    {ComerciosPorCiudad.map((comercio) => (
-        <div key={comercio.id} className="unComercio">
-            <p>{comercio.nombre}</p>
-            <p>{comercio.ciudad}</p>
-        </div>
-    ))}
-</div>
-
-<div className="lista">
-
-    {ComerciosPorActividad.map((comercio) => (
-
-        <div key={comercio.id} className="unComercio">
-            <p>{comercio.nombre}</p>
-            <p>{comercio.actividad}</p>
-        </div>
-
-    ))}
-</div>
-
-<div className="lista">
-                    
-    {ComerciosPorId.map((comercio) => (
-        <div key={comercio.id} className="unComercio">
-            <p>{comercio.nombre}</p>
-            <p>{comercio.id}</p>
-        </div>
-    ))}
-</div>
-
-*/
 
 function anonimo() {
 
@@ -106,6 +71,10 @@ function anonimo() {
                 </div>
 
             </div>
+
+            <button className="bg-white m-8 hover:bg-white-700 hover:scale-110 text-black font-bold py-2 px-4 rounded ">
+                <Link href="../registro">Registrarse</Link>
+            </button>
 
         </>
 
