439447b12b073374924fe9e26535717df86e9f58
pauss12 <paulamendezsv@gmail.com>
2023-12-09 13:24:42 +0100

    llamada a editar usuario y su funcion

---
+++ NEXT/practica3_next_final/app/componentes/editarUsuario.jsx
@@ -0,0 +1,93 @@
+import React, { useState } from 'react';
+
+function EditarUsuario() {
+    
+    const [usuario, setUsuario] = useState({
+        nombre: 'Nombre del Usuario',
+        ciudad: 'Ciudad del Usuario',
+        intereses: 'Intereses del Usuario',
+        recibirOfertas: true,
+    });
+
+    const handleInputChange = (e) => {
+        const { name, value, type, checked } = e.target;
+        const inputValue = type === 'checkbox' ? checked : value;
+
+        setUsuario((prevUsuario) => ({
+            ...prevUsuario,
+            [name]: inputValue,
+        }));
+    };
+
+    const handleActualizarUsuario = () => {
+        console.log('Datos del usuario actualizados:', usuario);
+        
+    };
+
+    return (
+        <div className="contenedorEditarUsuario">
+            <div className="container mx-auto mt-8">
+                <h2 className="text-2xl font-semibold mb-4">Editar Usuario</h2>
+                <form className="max-w-md">
+                    <div className="mb-4">
+                        <label className="block text-gray-700 text-sm font-bold mb-2">
+                            Nombre:
+                            <input
+                                type="text"
+                                name="nombre"
+                                value={usuario.nombre}
+                                onChange={handleInputChange}
+                                className="shadow appearance-none border rounded w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline"
+                            />
+                        </label>
+                    </div>
+                    <div className="mb-4">
+                        <label className="block text-gray-700 text-sm font-bold mb-2">
+                            Ciudad:
+                            <input
+                                type="text"
+                                name="ciudad"
+                                value={usuario.ciudad}
+                                onChange={handleInputChange}
+                                className="shadow appearance-none border rounded w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline"
+                            />
+                        </label>
+                    </div>
+                    <div className="mb-4">
+                        <label className="block text-gray-700 text-sm font-bold mb-2">
+                            Intereses:
+                            <input
+                                type="text"
+                                name="intereses"
+                                value={usuario.intereses}
+                                onChange={handleInputChange}
+                                className="shadow appearance-none border rounded w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline"
+                            />
+                        </label>
+                    </div>
+                    <div className="mb-4">
+                        <label className="block text-gray-700 text-sm font-bold mb-2">
+                            Recibir Ofertas:
+                            <input
+                                type="checkbox"
+                                name="recibirOfertas"
+                                checked={usuario.recibirOfertas}
+                                onChange={handleInputChange}
+                                className="mr-2 leading-tight"
+                            />
+                        </label>
+                    </div>
+                    <button
+                        type="button"
+                        onClick={handleActualizarUsuario}
+                        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                    >
+                        Actualizar Usuario
+                    </button>
+                </form>
+            </div>
+        </div>
+    );
+};
+
+export default EditarUsuario;
--- NEXT/practica3_next_final/app/usuario_registrado/page.jsx
+++ NEXT/practica3_next_final/app/usuario_registrado/page.jsx
@@ -1,6 +1,9 @@
 "use client"
 
 import { useState } from "react";
+import "../styles/usuarioRegistrado.css"
+
+import EditarUsuario from "../componentes/editarUsuario";
 
 function usuario_registrado() {
 
@@ -9,7 +12,7 @@ function usuario_registrado() {
 
     return (
         
-        hola
+        <EditarUsuario />
 
     );
 }
