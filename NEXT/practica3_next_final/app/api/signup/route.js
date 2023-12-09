479c3db055e74c1f179c31327db3f7f201ffa7a2
pauss12 <paulamendezsv@gmail.com>
2023-12-09 14:17:24 +0100

    actualizacion de la API

--- NEXT/practica3_next_final/app/api/signup/route.js
+++ NEXT/practica3_next_final/app/api/signup/route.js
@@ -1,30 +1,16 @@
 import { NextResponse } from 'next/server'
 import { readFileSync, writeFileSync } from 'fs';
 
-/*
-export async function POST(request) {
-    const data = await request.json()
-    try {
-        const users = JSON.parse(readFileSync("data/users.txt"))
-        writeFileSync("data/users.txt", JSON.stringify([...users, data], null, 2) + '\n')
-
-    } catch (e) {
-        writeFileSync("data/users.txt", JSON.stringify([data], null, 2) + '\n')
-        
-    }
-    return NextResponse.json({ message: "Guardando datos..." })
-}
-
-*/
 
 export async function POST(request) {
     const data = await request.json()
     try {
         const users = JSON.parse(readFileSync("data/users.txt"))
-        writeFileSync("data/users.txt", JSON.stringify([...users, data]))
+        //writeFileSync("data/users.txt", JSON.stringify([...users, data], null, 2) + '\n')
+        writeFileSync("data/users.txt", JSON.stringify([...users, data], null, 4))
 
     } catch (e) {
-        writeFileSync("data/users.txt", JSON.stringify([data]))
+        writeFileSync("data/users.txt", JSON.stringify([data], null, 4))
         
     }
     return NextResponse.json({ message: "Guardando datos..." })
