fc9c03f34f0f92ed53451f97b9878bace59fb508
pauss12 <paulamendezsv@gmail.com>
2023-12-12 01:53:22 +0100

    funcion POST del signup, la he unido a la de usuarios

--- NEXT/practica3_next_final/app/api/usuarios/route.js
+++ NEXT/practica3_next_final/app/api/usuarios/route.js
@@ -24,3 +24,17 @@ export async function DELETE(request) {
         console.log(e)
     }
 }
+
+export async function POST(request) {
+    const data = await request.json()
+    try {
+        const users = JSON.parse(readFileSync("data/users.txt"))
+        //writeFileSync("data/users.txt", JSON.stringify([...users, data], null, 2) + '\n')
+        writeFileSync("data/users.txt", JSON.stringify([...users, data], null, 4))
+
+    } catch (e) {
+        writeFileSync("data/users.txt", JSON.stringify([data], null, 4))
+
+    }
+    return NextResponse.json({ message: "Guardando datos..." })
+}
