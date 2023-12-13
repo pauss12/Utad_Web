a662ecf618559265c3f2c045e5126177a3ffdb0f
pauss12 <paulamendezsv@gmail.com>
2023-12-13 15:54:41 +0100

    actualizacion tarjeta usuario

--- NEXT/practica3_next_final/app/componentes/cartas/cartaUsuario.jsx
+++ NEXT/practica3_next_final/app/componentes/cartas/cartaUsuario.jsx
@@ -1,34 +1,33 @@
 import React from 'react';
 
-function cartaUsuario({ id, nombre, email, password, edad, ciudad, intereses, permiteOfertas, tipoUsuario}) {
+function cartaUsuario({ usuario, onChange }) {
+    
     return (
-        <div className="user-container">
-            <div className="user-info">
-                <div>
-                    <strong>ID:</strong> {id}
-                </div>
-                <h2>Nombre: {usuario_registrado.nombreUsuario}</h2>
-                <div>
-                    <strong>Email:</strong> {email}
-                </div>
-                <div>
-                    <strong>Edad:</strong> {edad}
-                </div>
-                <div>
-                    <strong>Ciudad:</strong> {ciudad}
-                </div>
-                <div>
-                    <strong>Intereses:</strong> {intereses}
-                </div>
-                <div>
-                    <strong>Permite Ofertas:</strong> {permiteOfertas ? 'Sí' : 'No'}
-                </div>
-                <div>
-                    <strong>Tipo:</strong> {tipo}
-                </div>
+
+        <>
+            <div className="bg-gray-200 rounded-2x1 ml-4 border border-black pl-4 pr-20 py-10 md:p-50 lg:px-30 my-2 shadow-md rounded">
+                    <h2>Nombre: {usuario.nombreUsuario}</h2>
+                    <p>ID: {usuario.idUsuario}</p> 
+                    <p>Email: {usuario.email}</p> 
+                    <p>Edad: {usuario.edad}</p> 
+                    <p>Ciudad: {usuario.edad}</p> 
+                    <p>Intereses: {usuario.intereses}</p> 
+                    <p>Permite ofertas: {usuario.permiteOfertas}</p> 
+                    <p>Tipo: {usuario.tipoUsuario}</p> 
             </div>
+                
+            {
+            onChange && (
+                <button className="bg-red-500 text-white rounded-md px-4 py-2"
+                    onClick={() => onChange(usuario)}>
+                    Editar Usuario
+                </button>
+            )}
+        
+            <hr></hr>
             
-        </div>
+        </>
+
     );
 }
 
