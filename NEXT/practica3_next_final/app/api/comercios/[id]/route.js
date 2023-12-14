5b982f2f3a35252655506c94fd94f9560fa5be59
pauss12 <paulamendezsv@gmail.com>
2023-12-14 01:17:05 +0100

    actualizacion del route

--- NEXT/practica3_next_final/app/api/comercios/[id]/route.js
+++ NEXT/practica3_next_final/app/api/comercios/[id]/route.js
@@ -3,17 +3,26 @@ import { readFileSync } from 'fs';
 
 
 export async function GET(request, { params }) {
+
     try {
+
         const users = JSON.parse(readFileSync("data/comercios.txt"));
+
         const user = users.find((comercio) => comercio.id === params.id);
 
         if (!user) {
+
             return NextResponse.json({ message: "Comercio not found", status: 404 });
+
         }
 
         return NextResponse.json({ user });
+
     } catch (error) {
-        console.error("Error fetching merchant:", error);
-        return NextResponse.json({ message: "Error fetching merchant", status: 500 });
+
+        console.error("Error fetching Comercio:", error);
+
+        return NextResponse.json({ message: "Error fetching Comercio", status: 500 });
+
     }
 }
