c1ad3d99628ba968236ec3f3aa6dabe1dd104d24
pauss12 <paulamendezsv@gmail.com>
2023-12-15 12:37:14 +0100

    actualizaciones editarComercio

--- NEXT/practica3_next_final/app/comercio/[idComercio]/page.jsx
+++ NEXT/practica3_next_final/app/comercio/[idComercio]/page.jsx
@@ -1,12 +1,49 @@
 "use client"
 
 import { useState, useEffect } from 'react';
+import EditarComercio from '@/app/componentes/EditarComercio';
+
+async function loadComercio(idComercio) {
+
+    const res = await fetch(`http://localhost:3000/api/comercios/${idComercio}`);
+
+    const data = await res.json()
+
+    return data.comercios
+}
 
 function Page({ params }) {
+
+    const [comercio, setComercio] = useState([]);
+
+    //Obtener el Comercio -------------------------------------
+    useEffect(() => {
+
+        const fetchData = async () => {
+
+            try {
+
+                const comercio = await loadComercio(params.idComercio);
+
+                setComercio(comercio);
+
+            } catch (error) {
+
+                console.error('Error al cargar el Comercio:', error);
+
+            }
+
+        };
+
+        fetchData();
+
+    }, []);
 
     return (
 
         <div className="flex flex-col ">
+
+            <EditarComercio comercio={comercio} />
 
             <div className="flex justify-center ">
                 
--- NEXT/practica3_next_final/app/componentes/EditarComercio.jsx
+++ NEXT/practica3_next_final/app/componentes/EditarComercio.jsx
@@ -1,7 +1,8 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useEffect } from 'react';
 
-function EditarComercio() {
+function EditarComercio( {comercio} ) {
 
+    /*
     //Const para la URL de la foto
     const [foto, setFoto] = useState("");
 
@@ -9,119 +10,78 @@ function EditarComercio() {
     const handleChangeFoto = (event) => {
         setFoto(event.target.value);
     };
+    */
+    
+    //Const para el comercio ------------------------
+    const [comercioIndividual, setComercioIndividual] = useState(comercio);
+
+    useEffect(() => {
+
+        setComercioIndividual(comercio);
+
+    }, [comercio]);
     
     return (
 
-         <>
+        <div className="contenedorEditarComercio">
 
-            <div className="flex min-h-full flex-1 flex-col justify-center px-6 lg:px-8">
-                <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-
-                    <h2 className="text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-                        Editar Comercio
-                    </h2>
-                </div>
-
-                <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-                    <form className="space-y-6" action="#" method="POST">
-                        <div>
-                            <div className="mt-2">
-                                Nombre Comercio
-                                <input
-                                    id="Nombre Comercio"
-                                    placeholder=" Nombre Comercio"
-                                    name="Nombre Comercio"
-                                    type="Nombre Comercio"
-                                    autoComplete="Nombre Comercio"
-                                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                                />
-                                
-                            </div>
-                        </div>
-
-                        <div>
-                            <div className="mt-2">
-                                CIF
-                                <input
-                                    id="CIF"
-                                    placeholder=" CIF"
-                                    name="CIF"
-                                    type="CIF"
-                                    autoComplete="CIF"
-                                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                                />
-                            </div>
-                        </div>
-
-                        <div>
-                            <div className="mt-2">
-                                Direccion
-                                <input
-                                    id="Direccion"
-                                    placeholder=" Direccion"
-                                    name="Direccion"
-                                    type="Direccion"
-                                    autoComplete="Direccion"
-                                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                                />
-                            </div>
-                        </div>
-
-                        <div>
-                            <div className="mt-2">
-                                Email
-                                <input
-                                    id="Email"
-                                    placeholder=" Email"
-                                    name="Email"
-                                    type="Email"
-                                    autoComplete="Email"
-                                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                                />
-                            </div>
-                        </div>
-
-                        <div>
-                            <div className="mt-2">
-                                Telefono
-                                <input
-                                    id="Telefono"
-                                    placeholder=" Telefono"
-                                    name="Telefono"
-                                    type="Telefono"
-                                    autoComplete="Telefono"
-                                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                                />
-                            </div>
-                        </div>
-
-                        <div className="mb-4">
-                            <label>Subir Foto:</label>
-                            <div>
-                                {/* Solo hay que subir el nombre del fichero */}
-                                <input
-                                    type="text"
-                                    value={foto}
-                                    onChange={handleChangeFoto}
-                                />
-                            </div>
-                        </div>
-
-                        <div>
-                            <button
-                                type="submit"
-                                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500  hover:scale-105 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                            >
-                                Editar Comercio
-                            </button>
-                        </div>
-
-
-                    </form>
-
-                </div>
+            <div className="container mx-auto mt-8">
+                <h2 className="text-2xl font-semibold mb-4">Editar Comercio</h2>
+                <form className="max-w-md">
+                    <div className="mb-4">
+                        <label className="block text-gray-700 text-sm font-bold mb-2">
+                            Nombre:
+                            <input
+                                type="text"
+                                name="nombreComercio"
+                                value={comercio.nombreComercio}
+                                className="shadow appearance-none border rounded w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline"
+                            />
+                        </label>
+                    </div>
+                    <div className="mb-4">
+                        <label className="block text-gray-700 text-sm font-bold mb-2">
+                            Ciudad:
+                            <input
+                                type="text"
+                                name="ciudadUsuario"
+                                value={usuario.ciudadUsuario}
+                                className="shadow appearance-none border rounded w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline"
+                            />
+                        </label>
+                    </div>
+                    <div className="mb-4">
+                        <label className="block text-gray-700 text-sm font-bold mb-2">
+                            Actividad:
+                            <input
+                                type="text"
+                                name="interesesUsuario"
+                                value={usuario.interesesUsuario}
+                                className="shadow appearance-none border rounded w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline"
+                            />
+                        </label>
+                    </div>
+                    <div className="mb-4">
+                        <label className="block text-gray-700 text-sm font-bold mb-2">
+                            Resumen:
+                            <input
+                                type="text"
+                                name="interesesUsuario"
+                                value={usuario.resumenComercio}
+                                className="shadow appearance-none border rounded w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline"
+                            />
+                        </label>
+                    </div>
+                    <button
+                        type="button"
+                        onClick={actualizarUsuario}
+                        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                    >
+                        Actualizar Usuario
+                    </button>
+                </form>
             </div>
-        </>
+        </div>
     );
 }
 
