6ffc33d21916969d6de9b29fc88f6f82de1d01e5
pauss12 <paulamendezsv@gmail.com>
2023-12-18 01:46:22 +0100

    Update page.jsx

--- NEXT/practica3_next_final/app/comercio/[idComercio]/page.jsx
+++ NEXT/practica3_next_final/app/comercio/[idComercio]/page.jsx
@@ -3,6 +3,8 @@
 import { useState, useEffect } from 'react';
 import EditarComercio from '@/app/componentes/EditarComercio';
 
+import { useRouter } from 'next/navigation';
+
 
 //Cargar el comercio --------------------------------------
 async function loadComercio(idComercio) {
@@ -14,33 +16,14 @@ async function loadComercio(idComercio) {
     return data.user
 }
 
-//Funcion para mostrar los datos del comercio ----------------------------
-function MostrarComercio({ comercio }) {
-
-    return (
-
-        <ul className="flex mt-5">
-
-            <li key={comercio.idComercio} className="bg-gray-200 rounded-2x1 ml-4 border border-black pl-4 pr-20 py-10 md:p-50 lg:px-30 my-2 shadow-md rounded">
-                <h2>Nombre: {comercio.nombreComercio}</h2>
-                <p>CIF: {comercio.cifComercio}</p>
-                <p>Dirección: {comercio.direccionComercio}</p>
-                <p>Email: {comercio.emailComercio}</p>
-                <p>Teléfono: {comercio.telefonoComercio}</p>
-                <p>Puntuación: {comercio.puntuacion}</p>
-                <p>Comentarios:{comercio.comentarios}</p>
-            
-            </li>
-
-            <hr></hr>
-
-        </ul>
-    );
-}
-
+//Funcion PAGE -------------------------------------------------------------------------
 function Page({ params }) {
 
-    const [comercio, setComercio] = useState([]);
+    //Usamos el router -----------------------------
+    const router = useRouter();
+
+    //Const para el comercio ------------------------
+    const [comercio, setComercio] = useState({});
 
     //Obtener el Comercio -------------------------------------
     useEffect(() => {
@@ -65,13 +48,61 @@ function Page({ params }) {
 
     }, []);
 
+    const estilo = {
+
+        height: '60vh',
+        marginLeft: '30px',
+        marginTop: '45px',
+        paddingLeft: '30px',
+        paddingBottom: '30px',
+        justifyContent: 'center',
+        paddingRight: '30px',
+        paddingTop: '30px',
+        alignItems: 'center',
+        border: '1px solid black',
+        borderRadius: '10px',
+        backgroundColor: '#fff',
+        boxShadow: '0 0 10px #ccc',
+
+    };
+
+    //Mostrar los datos no modificables del comercio (puntuacion, foto, textos, numero de puntuaciones, reseñas)
+    function showUnmodifiableData() {
+
+        return (
+
+            <div style={estilo}>
+                <strong>Puntuación:</strong> {comercio.puntuacion || 'N/A'}
+
+                <br />
+
+                <strong>Número de Puntuaciones:</strong> {comercio.numeroPuntuaciones || 0}
+
+                <br />
+
+                <strong>Reseñas:</strong> {comercio.comentarios || 'N/A'}
+
+                <br />
+
+                <strong>Foto:</strong>
+
+                {comercio.foto && <img src={comercio.fotos} alt="Foto del comercio" style={{ maxWidth: '100%', maxHeight: '100px' }} />}
+
+            </div>
+        );
+    }
+
+
+
+
+
     return (
 
         <div className="flex flex-row bg-blue-200">
 
-            <EditarComercio comercio={comercio} className="mr-5"/>
+            <EditarComercio comercio={comercio} className="mr-5" />
 
-            {MostrarComercio({ comercio: comercio })}
+            {showUnmodifiableData()}
 
             <button
                 className="absolute right-4 top-12 bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
